33186541d81f88440f87b4e99b06712d6d0fa84c
Mc-Jordan <jordannandjo11@gmail.com>
2025-03-05 11:38:43 +0100

    toujours en train de gerer la responsibilité sur les petits appareils

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,83 +56,11 @@ const displayIFrames = (element) => {
 	ifr.innerHTML = `<div class="flex justify-center items-center h-full"><span class="w-48 h-48 ${loader}"></span></div>`;
 
 	setTimeout(() => {
-		if (element == link[0] || element == link[11]) {
 			ifr.innerHTML = `<iframe
-								src="./1-chronometre/index.html"
+								src="${element.href}"
 								class="w-full h-full ${anime}"
 								name="iframe_a"
 								title="Iframe Example"
 							></iframe>`;
-		} else if (element == link[1] || element == link[12]) {
-			ifr.innerHTML = `<iframe
-								src="./2-toDoList/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[2] || element == link[13]) {
-			ifr.innerHTML = `<iframe
-								src="./3-jeux de saut/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[3] || element == link[14]) {
-			ifr.innerHTML = `<iframe
-								src="./4-jeux pierre papier ciseau/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[4] || element == link[15]) {
-			ifr.innerHTML = `<iframe
-								src="./5-verification de mot de passe/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[5] || element == link[16]) {
-			ifr.innerHTML = `<iframe
-								src="./6-jeu de calcul/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[6] || element == link[17]) {
-			ifr.innerHTML = `<iframe
-								src="./7-Code Bar generator/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[7] || element == link[18]) {
-			ifr.innerHTML = `<iframe
-								src="./8-Password Generator/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[8] || element == link[19]) {
-			ifr.innerHTML = `<iframe
-								src="./9-raccourcisseur-url/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[9] || element == link[20]) {
-			ifr.innerHTML = `<iframe
-								src="./10-creer des onglets/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		} else if (element == link[10] || element == link[21]) {
-			ifr.innerHTML = `<iframe
-								src="./11-Calculatrice/index.html"
-								class="w-full h-full ${anime}"
-								name="iframe_a"
-								title="Iframe Example"
-							></iframe>`;
-		}
 	}, 2500);
 };
